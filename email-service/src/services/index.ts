68e8a325fa31b913a658899caa739457a51e491c
njdifiore91 <nick@blitzy.com>
2025-05-20 22:54:58 -0400

    File: email-service/src/services/index.ts

---
+++ email-service/src/services/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @file index.ts
+ * @description Barrel file for exporting all service implementations
+ */
+
+// Export all services
+export * from './storage.service';
+export * from './email-monitor.service';
+export * from './message-queue.service';
+export * from './attachment-processor.service';
+export * from './virus-scanner.service';
