ecf8ed5b5df586eb5805840d6a9c909dc13cdded
njdifiore91 <nick@blitzy.com>
2025-05-20 22:55:09 -0400

    File: email-service/src/config/index.ts

---
+++ email-service/src/config/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @file index.ts
+ * @description Barrel file for exporting all configuration modules
+ */
+
+// Export all configuration modules
+export * from './app';
+export * from './imap';
+export * from './rabbitmq';
+export * from './s3';
+export * from './logger';
