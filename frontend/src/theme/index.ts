9e1c898439ee712957c2449f317d4bf3405d2319
njdifiore91 <nick@blitzy.com>
2025-05-20 21:10:17 -0400

    File: frontend/src/theme/index.ts

---
+++ frontend/src/theme/index.ts
@@ -0,0 +1,12 @@
+export * from './core';
+
+export * from './types';
+
+export * from './theme-config';
+
+export * from './theme-provider';
+
+// Funding-specific theme exports
+export * from './funding-colors';
+export * from './funding-components';
+export * from './funding-theme-provider';
